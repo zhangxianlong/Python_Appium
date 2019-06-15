47841408e640475c22feb5effec7e73833ab5439
zhangxianlong <zxl_73@126.com>
2019-06-15 10:00:40 +0800

    重构让代码，封装App.py

---
+++ page_object/page/App.py
@@ -0,0 +1,11 @@
+#encoding=utf-8
+
+from page_object.driver.AndroidClient import AndroidClient
+from page_object.page.MainPage import MainPage
+
+
+class App(object):
+    @classmethod
+    def main(self):
+        AndroidClient.restart_app()
+        return MainPage()
--- page_object/testcase/TestSelected.py
+++ page_object/testcase/TestSelected.py
@@ -1,10 +1,14 @@
 #coding=utf-8
 import pytest
 
+from page_object.page.App import App
 from page_object.page.MainPage import MainPage
 
 
 class TestSelected(object):
+
     def test_price(self):
-        main=MainPage()
-        assert main.gotoSelected().getPriceByName("科大讯飞")==28.83
+        assert App.main().gotoSelected().getPriceByName("科大讯飞")==28.83
+
+    def test_add_stock(self):
+        pass
